3f5368b21ba84b76d1e8fb27534641a51ae716f0
lisi <caoyunqi>
2019-05-21 09:27:43 +0800

    Initial commit

---
+++ src/main/java/com/xierenshuren/demokkm/DemokkmApplication.java
@@ -0,0 +1,13 @@
+package com.xierenshuren.demokkm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemokkmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemokkmApplication.class, args);
+    }
+
+}
